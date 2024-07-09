5b981d3a1727c5b87e8ab1b0204428de6d5d5685
Bryan García <bryan,garcia08@outlook.com>
2024-07-09 07:26:28 -0600

    Creando repositorio

---
+++ src/proyectodegit1/Proyectodegit1.java
@@ -0,0 +1,14 @@
+
+package proyectodegit1;
+
+
+public class Proyectodegit1 {
+    
+    public static void main(String[] args) {
+        System.out.println("Mi proyecto");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
